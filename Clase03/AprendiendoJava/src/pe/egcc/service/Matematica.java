52c624ac31b38b7483e7be79dc710123a59c1c4b
Gustavo Coronel <gcoronelc@gmail.com>
2016-03-05 16:10:39 -0500

    Clase 02

---
+++ Clase03/AprendiendoJava/src/pe/egcc/service/Matematica.java
@@ -0,0 +1,30 @@
+package pe.egcc.service;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class Matematica {
+
+  public int promediar(int n1, int n2) {
+    System.err.println("Caso 1");
+    int pr;
+    pr = (n1 + n2) / 2;
+    return pr;
+  }
+
+  public double promediar(double n1, double n2) {
+    System.err.println("Caso 2");
+    double pr;
+    pr = (n1 + n2) / 2;
+    return pr;
+  }
+
+  public float promediar(float n1, float n2) {
+    System.err.println("Caso 3");
+    float pr;
+    pr = (n1 + n2) / 2;
+    return pr;
+  }
+  
+}
